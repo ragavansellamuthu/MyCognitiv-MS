31d4fb19134037477b31ef7726cad0ca16829bd7
Ragavan Sellamuthu <155799199+ragavansellamuthu@users.noreply.github.com>
2024-06-03 20:19:19 +0530

    Created Professor Service

---
+++ mycognitiv-microservices/professor-service/src/test/java/com/techietact/mycognitiv/professor_service/ProfessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techietact.mycognitiv.professor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
