ee55c3412deeb919bf515b8d61413f5ccc7beaa8
Ragavan Sellamuthu <155799199+ragavansellamuthu@users.noreply.github.com>
2024-06-09 13:09:10 +0530

    Implemented Configuration Server

---
+++ mycognitiv-microservices/configuration-server/src/test/java/com/techietact/mycognitiv/configuration/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techietact.mycognitiv.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
