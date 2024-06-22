16497665fe0d0e0fe84b5b8f40e963ca299398dc
Ragavan Sellamuthu <155799199+ragavansellamuthu@users.noreply.github.com>
2024-06-22 11:28:07 +0530

    Implemented progress bar

--- mycognitiv-angular/src/app/component/candidate/candidate-details/candidate-details.component.ts
+++ mycognitiv-angular/src/app/component/candidate/candidate-details/candidate-details.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DeleteComponent } from '../../common/delete/delete.component';
+import { Candidate } from 'src/app/model/candidate/candidate';
+import { CandidateService } from 'src/app/service/candidate/candidate.service';
+import { NotificationService } from 'src/app/service/notification/notification.service';
 
 @Component({
   selector: 'app-candidate-details',
   templateUrl: './candidate-details.component.html',
   styleUrls: ['./candidate-details.component.css']
 })
-export class CandidateDetailsComponent {
+export class CandidateDetailsComponent implements OnInit {
 
+  candidate !: Candidate ;
+
+  constructor (
+
+    public dialogRef: MatDialogRef<DeleteComponent>,
+
+    @Inject(MAT_DIALOG_DATA) 
+    public data: any,
+
+    private candidateService : CandidateService,
+    private notificationService : NotificationService 
+    
+  ) {
+    this.candidate = new Candidate() ;
+  }
+
+  ngOnInit(): void {
+    this.candidateService.viewCandidate(this.data.candidateId).subscribe({
+      next : (candidate) => {
+        if(candidate){
+          this.candidate = candidate ;
+        }
+      },
+      error : (err) => {
+        console.error(err);
+        this.notificationService.notify('Please try again later','OK');
+      }
+    });
+  }
+
+  download() {
+    debugger
+  }
+  
 }
